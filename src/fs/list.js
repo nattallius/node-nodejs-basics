7cbbee85327fd9dd53cd8a8f497828af4c5bb6b5
Natallia <nattallius@gmail.com>
2024-01-30 00:48:33 +0100

    File system practice

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const currentFilename = fileURLToPath(import.meta.url);
+const filesDirectory = path.join(path.dirname(currentFilename), '/files')
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await fs.readdir(filesDirectory);
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const currentFilename = fileURLToPath(import.meta.url);
+const fileToRead = path.join(path.dirname(currentFilename), '/files/fileToRea3d.txt');
 const read = async () => {
-    // Write your code here 
+    try {
+        const content = await fs.readFile(fileToRead, {encoding: 'utf8'});
+        console.log(content);
+    } catch (e) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
