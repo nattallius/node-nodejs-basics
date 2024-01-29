8e58afa7464b96226a42405bd6982ba4aa6ccefd
Natallia <nattallius@gmail.com>
2024-01-30 00:50:05 +0100

    Hash practice

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import {fileURLToPath} from "url";
+import path from "path";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const hashFilePath = `${dirname}/files/fileToCalculateHashFor.txt`;
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fd = fs.createReadStream(hashFilePath);
+    const hash = crypto.createHash('sha256');
+    hash.setEncoding('hex');
+
+    fd.on('end', function () {
+        hash.end();
+        console.log(hash.read());
+    });
+
+    fd.pipe(hash);
 };
 
 await calculateHash();
