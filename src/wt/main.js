bdbbf64530ac8f2a4fbccd30d0ed5a42756bff4c
Natallia <nattallius@gmail.com>
2024-01-30 00:51:24 +0100

    Worker Threads practice

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import {cpus} from 'os';
+import {fileURLToPath} from "url";
+import path from "path";
+import {Worker} from 'worker_threads';
+
+
+const filename = fileURLToPath(import.meta.url);
+const workerPath = `${path.dirname(filename)}/worker.js`;
 const performCalculations = async () => {
-    // Write your code here
-};
+    const cpuNumber = cpus().length;
+    const workersArray = [];
+    for (let i = 0; i < cpuNumber; i++) {
+        const promise = new Promise((resolve) => {
+            const worker = new Worker(workerPath, {workerData: i + 10});
+            worker.on('message', (res) => resolve({status: 'resolved', data: res}));
+            worker.on('error', () => resolve({status: 'error', data: null}));
+        });
+        workersArray.push(promise);
+    }
+    const resul = await Promise.all(workersArray);
+    console.log(resul);
+}
 
 await performCalculations();
