94bd08ae5e047421b95273537e6b0dc9d8d80f98
Natallia <nattallius@gmail.com>
2024-01-30 00:51:40 +0100

    Child Processes practice

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import {fork} from 'child_process';
+import {fileURLToPath} from "url";
+import path from "path";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const scriptPath = `${dirname}/files/script.js`;
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(scriptPath, args, {silent: true});
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(  ['someArgument1', 'someArgument2'] );
